5cf4938871f5b9ab3a5c6fbf4411581eb2599b12
Seonggwan Lee <seonggwan.lee@gmail.com>
2019-07-02 16:03:06 +0900

    Forward ref

--- src/input.jsx
+++ src/input.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Input = ({ name, ...props }) => {
+const Input = React.forwardRe(({ name, ...props }, ref) => {
   return (
     <div>
       <label htmlFor={name}>{name}</label>
-      <input type="text" name={name} {...props} />
+      <input type="text" name={name} ref={ref} {...props} />
     </div>
   );
-};
+});
 
 export default Input;
