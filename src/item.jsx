9423db8e81eae961048fda6daa0b1d0084a34034
Seonggwan Lee <seonggwan.lee@gmail.com>
2019-07-02 16:02:13 +0900

    Add Ref callback

--- src/item.jsx
+++ src/item.jsx
@@ -3,13 +3,19 @@ import Input from "./input";
 
 class Item extends Component {
   state = {};
+  inputRef = {};
+
+  // ref callback
+  setInputRef = element => {
+    this.inputRef[element.name] = element;
+  };
 
   render() {
     return (
       <div>
         Item
-        <Input name={"name"} />
-        <Input name={"brand"} />
+        <Input name={"name"} ref={this.setInputRef} />
+        <Input name={"brand"} ref={this.setInputRef} />
         <button>Focus name</button>
         <button>Focus brand</button>
       </div>
